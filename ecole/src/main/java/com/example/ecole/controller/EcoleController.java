bce760f25cf1d0219667ea783a24304a024fc57d
Bilal1960 <bilal45001@hotmail.com>
2023-07-27 06:21:37 +0200

    implémentation model crud: update dans le controller Ecole

--- ecole/src/main/java/com/example/ecole/controller/EcoleController.java
+++ ecole/src/main/java/com/example/ecole/controller/EcoleController.java
@@ -2,16 +2,13 @@ package com.example.ecole.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import com.example.ecole.repository.EcoleRepository;
 import com.example.ecole.models.Ecole;
-
 import java.util.UUID;
-
-
-
 @RestController
 @RequestMapping("/add/ecoles")
 public class EcoleController {
@@ -24,7 +21,7 @@ public class EcoleController {
     }
 
     @GetMapping("/ecole")
-    public ResponseEntity<Ecole> getEcole(Authentication authentication) {
+    public ResponseEntity<Ecole> getEcole() {
         UUID id = UUID.fromString("04ece6ce-2690-4152-a5c9-09d40d5891b7");
 
         Ecole ecole = ecoleRepository.findEcoleById(id); // Assurez-vous d'avoir une méthode pour récupérer l'école unique.
@@ -35,6 +32,35 @@ public class EcoleController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PutMapping("/ecole/{id}")
+    public ResponseEntity<Ecole> updateEcole(@PathVariable UUID id, @RequestBody Ecole updatedEcole, Authentication authentication) {
+        if (hasAuthority(authentication, "SCOPE_write:ecole")) {
+
+            Ecole existingEcole = ecoleRepository.findEcoleById(id);
+            if (existingEcole == null) {
+                logger.warn("Échec, école introuvable");
+                return ResponseEntity.notFound().build();
+            }
+
+            existingEcole.setAdresse(updatedEcole.getAdresse());
+            existingEcole.setMail(updatedEcole.getMail());
+            existingEcole.setNumber(updatedEcole.getNumber());
+            existingEcole.setType(updatedEcole.getType());
+
+            Ecole updatedSchool = ecoleRepository.save(existingEcole);
+            logger.info("Succès de la mise à jour de l'école");
+            return ResponseEntity.ok(updatedSchool);
+        }
+        logger.warn("Attention, vous n'avez pas la bonne permission");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    private static boolean hasAuthority(Authentication authentication, String expectedAuthority) {
+        logger.info("vérifier l'autorité de permission", expectedAuthority);
+        return authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals(expectedAuthority));
+    }
+
 }
 
 
