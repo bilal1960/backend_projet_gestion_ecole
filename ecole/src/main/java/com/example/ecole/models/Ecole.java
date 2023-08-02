12a99046209d6b5f4ba3c53f09d170935f2f4e94
Bilal1960 <bilal45001@hotmail.com>
2023-08-02 20:15:50 +0200

    indentation model Ecole

--- ecole/src/main/java/com/example/ecole/models/Ecole.java
+++ ecole/src/main/java/com/example/ecole/models/Ecole.java
@@ -7,28 +7,30 @@ import java.util.UUID;
 @Entity
 @Table(name="ecole")
 public class Ecole {
+
     @Id
-    @Column(name="id",nullable = false)
+    @Column(name="id", nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(length=30, nullable=false)
+    @Column(length = 30, nullable = false)
     private String nom;
+
     @Pattern(regexp = "^[A-Za-z][A-Za-z\\d\\s./]*$", message = "Entrer une adresse valide")
-    @Column(length=30, nullable=false)
+    @Column(length = 30, nullable = false)
     private String adresse;
 
     @Pattern(regexp = "secondaire(\\s?/\\s?supérieur)?", message = "Le type doit être 'secondaire' ou 'secondaire/supérieur'")
-    @Column(length=30, nullable=false)
-    private  String type;
+    @Column(length = 30, nullable = false)
+    private String type;
+
     @Pattern(regexp = ".*@.*\\..*", message = "Entrer une adresse e-mail valide")
     @Column(length = 30, nullable = false)
     private String mail;
+
     @Pattern(regexp = "(\\+32\\s?)?\\d{1,}(\\s?\\d{2}){1,}", message = "Le numéro de téléphone doit être au format +32 XXX XX XX XX ou XXX XX XX XX")
     @Column(length = 30, nullable = false)
-    private  String number;
-
-
+    private String number;
 
     public Ecole(String nom, String adresse, String mail, String number, String type) {
         this.nom = nom;
@@ -50,7 +52,6 @@ public class Ecole {
         return adresse;
     }
 
-
     public String getType() {
         return type;
     }
@@ -67,12 +68,11 @@ public class Ecole {
         this.adresse = adresse;
     }
 
-
     public void setType(String type) {
         this.type = type;
     }
 
-    public  Ecole(){
+    public Ecole() {
 
     }
 
@@ -92,4 +92,3 @@ public class Ecole {
         this.mail = mail;
     }
 }
-
