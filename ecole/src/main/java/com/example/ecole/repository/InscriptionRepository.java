9295983432419653f18177be242a3e790e88d6f1
Bilal1960 <bilal45001@hotmail.com>
2023-08-02 20:40:05 +0200

    indentation Repository: Inscription

--- ecole/src/main/java/com/example/ecole/repository/InscriptionRepository.java
+++ ecole/src/main/java/com/example/ecole/repository/InscriptionRepository.java
@@ -1,12 +1,13 @@
 package com.example.ecole.repository;
+
 import com.example.ecole.models.Inscription;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+
 public interface InscriptionRepository extends JpaRepository<Inscription, UUID> {
     @EntityGraph(attributePaths = "personne_id")
     Page<Inscription> findAll(Pageable pageable);
-
 }
